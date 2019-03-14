30de374fca3535bcec9cfbe3207fcf04a950635f
aibek <aibek9898@gmail.com>
2019-03-14 10:32:43 +0600

    Create Controller

--- src/main/java/kz/aibek/example/Application.java
+++ src/main/java/kz/aibek/example/Application.java
@@ -1,10 +1,30 @@
 package kz.aibek.example;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.annotation.MultipartConfig;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 public class Application {
+
+    @Bean
+    MultipartConfigElement multipartConfigElement(){
+        MultipartConfigFactory factory= new MultipartConfigFactory();
+        factory.setMaxFileSize("128KB");
+        factory.setMaxRequestSize("128KB");
+        return factory.createMultipartConfig();
+    }
+
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
---
+++ src/main/java/kz/aibek/example/FileUploadController.java
@@ -0,0 +1,24 @@
+package kz.aibek.example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+public class FileUploadController {
+    @RequestMapping(value="/upload", method= RequestMethod.GET)
+    public @ResponseBody String handleFileUpload(@RequestParam("name") String name, @RequestParam("file")MultipartFile file){
+        if(!file.isEmpty()) {
+            try {
+                return "Удачно загрузили "+name+" в "+name+"-upload !";
+            } catch (Exception e) {
+                return "Не удалось загрузить " + name + " в " + name + " => " + e.getMessage();
+            }
+        }else{
+            return "Файл "+name+" пустой";
+        }
+    }
+}
