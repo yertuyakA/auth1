20bc13f2d7aa3b75e2911b4da55044ce77b7caae
aibek <aibek9898@gmail.com>
2019-03-15 11:29:00 +0600

    commit changes

--- src/main/java/kz/aibek/example/Application.java
+++ src/main/java/kz/aibek/example/Application.java
@@ -1,21 +1,21 @@
 package kz.aibek.example;
 
+
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
 import javax.servlet.MultipartConfigElement;
-import javax.servlet.annotation.MultipartConfig;
+
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
 public class Application {
-
+/*
     @Bean
     MultipartConfigElement multipartConfigElement(){
         MultipartConfigFactory factory= new MultipartConfigFactory();
@@ -23,8 +23,11 @@ public class Application {
         factory.setMaxRequestSize("128KB");
         return factory.createMultipartConfig();
     }
+*/
+
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
 }
---
+++ src/main/java/kz/aibek/example/controller/FileUploadController.java
@@ -0,0 +1,52 @@
+package kz.aibek.example.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
+@Controller
+public class FileUploadController {
+
+    private static String UPLOADED_FOLDER = "/home/aibek/";
+
+    @GetMapping("/")
+    public String index(){
+        return "upload";
+    }
+
+    @PostMapping("/upload")
+    public String singleFileUpload(@RequestParam("file")MultipartFile file, RedirectAttributes redirectAttributes) {
+        if (file.isEmpty()) {
+            redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
+            return "redirect:uploadStatus";
+        }
+        try{
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(UPLOADED_FOLDER+file.getOriginalFilename());
+            Files.write(path, bytes);
+            redirectAttributes.addFlashAttribute("message", "You succesfully uploaded " + file.getOriginalFilename()+"'");
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return "redirect:/uploadStatus";
+
+    }
+
+    @GetMapping("/uploadStatus")
+    public String uploadStatus(){
+        return "uploadStatus";
+    }
+
+
+}
