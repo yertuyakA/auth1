9a482877f2acaf391311b31566d4c9a78a3a3a09
aibek <aibek9898@gmail.com>
2019-03-15 11:30:29 +0600

    commit changes

--- src/main/java/kz/aibek/example/Application.java
+++ src/main/java/kz/aibek/example/Application.java
@@ -1,20 +1,27 @@
 package kz.aibek.example;
 
 
+import org.apache.coyote.http11.AbstractHttp11Protocol;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import javax.servlet.MultipartConfigElement;
+
 
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
 public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 /*
     @Bean
     MultipartConfigElement multipartConfigElement(){
@@ -24,10 +31,24 @@ public class Application {
         return factory.createMultipartConfig();
     }
 */
+    private int maxUploadSizeInMb = 10*1024*1024;
+
+    @Bean
+    public TomcatEmbeddedServletContainerFactory tomcatEmbedded() {
+
+    TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+
+    tomcat.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> {
+        if ((connector.getProtocolHandler() instanceof AbstractHttp11Protocol<?>)) {
+            //-1 means unlimited
+            ((AbstractHttp11Protocol<?>) connector.getProtocolHandler()).setMaxSwallowSize(-1);
+        }
+    });
+
+    return tomcat;
+
+}
 
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
 
 }
